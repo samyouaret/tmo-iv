a36d80c3089e7f09e9110da158e3983611378f26
samyouaret <samyouaret13@gmail.com>
2024-04-18 16:10:50 +0100

    feat(auth): use config service to sign jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { UsersService } from 'src/users/users.service';
 import { AccessTokenType } from './types/AccessTokenType';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayloadType } from './types/JwtPayloadType';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwtService: JwtService,
     private usersService: UsersService,
+    private readonly configService: ConfigService,
   ) {}
 
   async login(email: string, password: string): Promise<AccessTokenType> {
@@ -16,7 +18,10 @@ export class AuthService {
     const payload: JwtPayloadType = { sub: user.id, aud: user.role };
 
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: await this.jwtService.signAsync(payload, {
+        secret: this.configService.get('JWT_SECRET'),
+        expiresIn: '15m',
+      }),
     };
   }
 }
