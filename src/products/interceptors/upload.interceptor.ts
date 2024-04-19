2729175de146838e4bca0837a5431226ca7de4b9
samyouaret <samyouaret13@gmail.com>
2024-04-19 22:45:58 +0100

    feat(products): add file upload interceptor

---
+++ src/products/interceptors/upload.interceptor.ts
@@ -0,0 +1,21 @@
+import { UseInterceptors, applyDecorators } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { randomBytes } from 'crypto';
+import { extname } from 'path';
+
+export function UseFileUpload(uploadField: string) {
+  return applyDecorators(
+    UseInterceptors(
+      FileInterceptor(uploadField, {
+        storage: diskStorage({
+          destination: './uploads',
+          filename: (_req, file, cb) => {
+            const randomName = randomBytes(16).toString('hex');
+            cb(null, `${randomName}${extname(file.originalname)}`);
+          },
+        }),
+      }),
+    ),
+  );
+}
