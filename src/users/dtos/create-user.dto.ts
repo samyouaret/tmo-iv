8cdf303b9014ad5ae27ce45f90accdcfab106e22
samyouaret <samyouaret13@gmail.com>
2024-04-19 18:32:56 +0100

    feat(users): document create user dto with swagger

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,15 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Samy',
+    description: 'First name or given name',
+  })
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty({
+    example: 'Ouaret',
+    description: 'last name or family name',
+  })
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty({
+    example: 'samy@gmail.com',
+    description: 'User email',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
