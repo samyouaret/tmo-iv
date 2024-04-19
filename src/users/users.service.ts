fdfaa9567c1b500cc5c078523d03a55c4f22adc6
samyouaret <samyouaret13@gmail.com>
2024-04-19 18:01:18 +0100

    feat(users): use user repository instead of plain objects

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,50 +1,62 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { randomUUID } from 'crypto';
-import { UserRoleType } from './types/UserRoleType';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserType } from 'src/users/types/UserType';
 import * as bcrypt from 'bcrypt';
-import { INVALID_CREDENTIALS } from './errors';
+import { INVALID_CREDENTIALS, USER_ALREADY_EXISTS } from './errors';
+import { User } from './entities/users.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  async verifyIdentity(
-    email: string,
-    password: string,
-  ): Promise<UserType<string>> {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(input: Omit<UserType, 'id'>): Promise<UserType> {
+    await this.ensureUserNotExists(input.email);
+    const newUser = this.usersRepository.create(input);
+    newUser.password = await this.hashPassword(input.password);
+
+    await this.usersRepository.save(newUser);
+    delete newUser.password;
+
+    return newUser;
+  }
+
+  private async ensureUserNotExists(email: string) {
+    const user = await this.findByEmail(email);
+    if (user != null) {
+      throw new BadRequestException(USER_ALREADY_EXISTS);
+    }
+  }
+
+  async verifyIdentity(email: string, password: string): Promise<UserType> {
     const user = await this.findByEmail(email);
     if (user == null) {
       throw new UnauthorizedException(INVALID_CREDENTIALS);
     }
+
     await this.ensurePasswordMatch(user, password);
 
     return user;
   }
 
-  async findByEmail(email: string): Promise<UserType<string>> {
-    return {
-      email: email,
-      password: '$2b$10$HhdyTxLlghypibz21hgnhezF6hJ2ng4IFAhvG5Nw.UDlMZMnJjR0W',
-      firstName: 'Dom',
-      lastName: 'Brocks',
-      id: randomUUID(),
-      role: UserRoleType.GUEST,
-    };
+  async findByEmail(email: string): Promise<UserType | null> {
+    return this.usersRepository.findOne({ where: { email } });
   }
 
-  private async ensurePasswordMatch(user: UserType<string>, password: string) {
+  private async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+
+  private async ensurePasswordMatch(user: UserType, password: string) {
     if ((await bcrypt.compare(password, user?.password)) === false) {
       throw new UnauthorizedException(INVALID_CREDENTIALS);
     }
-  }
-
-  async findByID(id: string): Promise<UserType<string>> {
-    return {
-      email: 'dombrocks@mail.com',
-      password: '123456',
-      firstName: 'Dom',
-      lastName: 'Brocks',
-      id,
-      role: UserRoleType.GUEST,
-    };
   }
 }
