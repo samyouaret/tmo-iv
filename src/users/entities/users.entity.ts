278c7d41d84ba68ad4af955e083d49699be868ff
samyouaret <samyouaret13@gmail.com>
2024-04-19 18:33:39 +0100

    feat(users): document user entity with swagger

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,24 +1,32 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { UserRoleType } from '../types/UserRoleType';
 import { UserType } from '../types/UserType';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User implements UserType {
+  @ApiProperty({
+    example: 101,
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column()
   firstName: string;
 
+  @ApiProperty()
   @Column()
   lastName: string;
 
+  @ApiProperty()
   @Column()
   email: string;
 
   @Column()
   password: string;
 
+  @ApiProperty({ enum: UserRoleType, default: UserRoleType.GUEST })
   @Column({ type: 'enum', enum: UserRoleType, default: UserRoleType.GUEST })
   role: UserRoleType;
 }
