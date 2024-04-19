30bcf79f79ca3d8b896c730a53f91d70246db6b3
samyouaret <samyouaret13@gmail.com>
2024-04-19 18:34:00 +0100

    feat(users): document create new user with swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,10 +2,19 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UserRoleType } from './types/UserRoleType';
+import { ApiResponse } from '@nestjs/swagger';
+import { User } from './entities/users.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @ApiResponse({
+    status: 201,
+    description: 'A new user is registered',
+    type: User,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create({
