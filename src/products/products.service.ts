373c1b90e5369406531d4098648a3bb636831efc
samyouaret <samyouaret13@gmail.com>
2024-04-19 23:20:13 +0100

    feat(products): add update product methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as fs from 'fs';
 
 @Injectable()
 export class ProductsService {
@@ -14,5 +15,30 @@ export class ProductsService {
     const product = this.productsRepository.create(input);
 
     return this.productsRepository.save(product);
+  }
+
+  async update(id: number, input: Omit<Product, 'id'>): Promise<Product> {
+    const product = await this.productsRepository.findOne({ where: { id } });
+    if (product == null) {
+      throw new NotFoundException();
+    }
+    if (product.image !== input.image) {
+      //  delete image from storage
+      await this.deleteImage(product.image);
+    }
+
+    Object.assign(product, input);
+    return this.productsRepository.save(product);
+  }
+
+  async deleteImage(path: string): Promise<true | NodeJS.ErrnoException> {
+    return new Promise((resolve, reject) => {
+      fs.unlink(path, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(true);
+      });
+    });
   }
 }
