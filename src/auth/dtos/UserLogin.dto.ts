614c41f7ef4ee448800a9c8c6b47f5f3dc08f777
samyouaret <samyouaret13@gmail.com>
2024-04-20 15:18:56 +0100

    feat(auth): add user login dto with validation and swagger

--- src/auth/dtos/UserLogin.dto.ts
+++ src/auth/dtos/UserLogin.dto.ts
@@ -1,4 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class UserLoginDto {
+  @ApiProperty()
+  @IsEmail()
   email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
   password: string;
 }
