8aade39c1ebc005d3962a4dc30452ab01df550fc
samyouaret <samyouaret13@gmail.com>
2024-04-20 19:56:35 +0100

    chore: remove hardcoded app port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Tmo E-Commerce API')
@@ -14,9 +16,10 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('APP_PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
