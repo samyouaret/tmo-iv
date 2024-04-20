32354484e66fb7cd5251b2c04b31df376564b5b9
samyouaret <samyouaret13@gmail.com>
2024-04-20 15:19:55 +0100

    feat(auth): add swagger to login handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from './dtos/UserLogin.dto';
+import { AccessTokenDto } from './dtos/AccessToken.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiResponse({
+    status: 200,
+    description: 'Sign in with email and password',
+    type: AccessTokenDto,
+  })
+  @ApiResponse({ status: 400 })
   login(@Body() input: UserLoginDto) {
     return this.authService.login(input.email, input.password);
   }
