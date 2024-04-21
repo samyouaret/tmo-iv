f67c3ac20ffd75ef5e3618d5cd9b95cb040d8c1b
samyouaret <samyouaret13@gmail.com>
2024-04-21 10:35:43 +0100

    chore(products): import auth module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { FileStorage } from '../common/storage/FileStorage';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), AuthModule],
   providers: [ProductsService, FileStorage],
   controllers: [ProductsController],
 })
