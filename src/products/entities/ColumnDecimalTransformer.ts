6f8355f9e4b5bec54fd5af0fc5351184c2a8d9c1
samyouaret <samyouaret13@gmail.com>
2024-04-21 12:52:44 +0100

    refactor: clarify decimal transfomer utility

--- src/products/entities/ColumnDecimalTransformer.ts
+++ src/products/entities/ColumnDecimalTransformer.ts
@@ -1,9 +1,16 @@
-/// ColumnNumericTransformer
-export class ColumnDecimalTransformer {
-  to(data: number): number {
-    return data;
+import { ValueTransformer } from 'typeorm';
+
+export class ColumnDecimalTransformer implements ValueTransformer {
+  /**
+   * Used to marshal Decimal when writing to the database.
+   */
+  to(decimal: number): string | null {
+    return decimal?.toString();
   }
-  from(data: string): number {
-    return parseFloat(data);
+  /**
+   * Used to unmarshal Decimal when reading from the database.
+   */
+  from(decimal?: string): number | null {
+    return decimal ? parseFloat(decimal) : null;
   }
 }
